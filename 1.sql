50b850bc2a1bf4d015d67d169d3f70a1db51abfb
Локтеев Илья <LokteevIA@yandex.ru>
2026-01-14 00:33:35 +0500

    fix

--- 1.sql
+++ 1.sql
@@ -9,3 +9,10 @@ select * from "ImportTransactionItems"
 select * from "Transactions" where --"TransactionTypeId" = 21 and
                                    "CompanyId"=2
                              order by "DateTime" desc
+
+
+select * from "CashBoxes";
+
+select * from "ImportTransactionCompanyPurposes"
+
+select * from "Users" where "CompanyId"=2 and "UserName" like '%Mosta%'
