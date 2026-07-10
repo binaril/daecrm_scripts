0d5bc03c10a982f36398b41d6e32aa949435ca3a
Локтеев Илья <LokteevIA@yandex.ru>
2026-07-10 17:31:03 +0500

    fix

--- migrations_daecrm/current.sql
+++ migrations_daecrm/current.sql
@@ -4,6 +4,3 @@ select * from "__EFMigrationsHistory" order by "MigrationId" desc
 GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public TO limousine;
 
 
-20260619135912_AddTransactionAggregatorOrderId
-
-20260622115851_AddPnlReadAccess
