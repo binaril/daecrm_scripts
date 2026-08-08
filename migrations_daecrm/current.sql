c6c82450ff2e0433d6280bf26981867da50f3266
Локтеев Илья <LokteevIA@yandex.ru>
2026-08-08 12:54:31 +0500

    fix

--- migrations_daecrm/current.sql
+++ migrations_daecrm/current.sql
@@ -1,5 +1,11 @@
 select * from "__EFMigrationsHistory" order by "MigrationId" desc
 
-
+--у
 GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public TO limousine;
+GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA public TO limousine;
 
+-- чтобы это не повторялось на следующей миграции:
+ALTER DEFAULT PRIVILEGES FOR ROLE postgres IN SCHEMA public
+  GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO limousine;
+ALTER DEFAULT PRIVILEGES FOR ROLE postgres IN SCHEMA public
+  GRANT USAGE, SELECT ON SEQUENCES TO limousine;
