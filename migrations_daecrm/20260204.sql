1eec1def1b6ecf4df777e4a0e45d1b805cabc085
Илья <lokteevia@yandex.ru>
2026-08-23 18:48:23 +0500

    fix

--- migrations_daecrm/20260204.sql
+++
@@ -1,9 +0,0 @@
-START TRANSACTION;
-INSERT INTO "PurposeReportType" ("Id", "Name")
-VALUES (4, 'Electric charge');
-
-INSERT INTO "__EFMigrationsHistory" ("MigrationId", "ProductVersion")
-VALUES ('20260204163915_add_purpose_type_electric', '10.0.1');
-
-COMMIT;
-
